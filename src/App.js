fb2c8bb1ad77ebd4995a0029711d369cd7b3522c
Kurt Van Etten <kurt@vanetten.us>
2019-10-21 19:16:38 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -25,10 +25,12 @@ const App = () => {
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
+
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
+    
   };
 
   return (
